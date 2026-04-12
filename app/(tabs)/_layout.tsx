66b2f3a5402b86112ac616ed1dd8eda46a3abd8a
jmackowiak <jmackowak@gmail.com>
2026-04-12 21:56:07 +0200

    add biome

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,46 +2,46 @@ import { Ionicons } from '@expo/vector-icons'
 import { Tabs } from 'expo-router'
 
 export default function TabLayout() {
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: '#ffd33d',
-        headerStyle: {
-          backgroundColor: '#25292e',
-        },
-        headerShadowVisible: false,
-        headerTintColor: '#fff',
-        tabBarStyle: {
-          backgroundColor: '#25292e',
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? 'home-sharp' : 'home-outline'}
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="about"
-        options={{
-          title: 'About',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={
-                focused ? 'information-circle' : 'information-circle-outline'
-              }
-              color={color}
-            />
-          ),
-        }}
-      />
-    </Tabs>
-  )
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: '#ffd33d',
+				headerStyle: {
+					backgroundColor: 'black',
+				},
+				headerShadowVisible: false,
+				headerTintColor: '#fff',
+				tabBarStyle: {
+					backgroundColor: 'black',
+				},
+			}}
+		>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: 'Home',
+					tabBarIcon: ({ color, focused }) => (
+						<Ionicons
+							color={color}
+							name={focused ? 'home-sharp' : 'home-outline'}
+						/>
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="about"
+				options={{
+					title: 'About',
+					tabBarIcon: ({ color, focused }) => (
+						<Ionicons
+							color={color}
+							name={
+								focused ? 'information-circle' : 'information-circle-outline'
+							}
+						/>
+					),
+				}}
+			/>
+		</Tabs>
+	)
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native'
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+				backgroundColor: 'black',
+			}}
+		>
+			<Text style={{ color: 'white' }}>
+				Edit app/index.tsx to edit this screen.
+			</Text>
+		</View>
+	)
 }
