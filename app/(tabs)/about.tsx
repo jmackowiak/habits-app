294fd6d32af427c1cde0628f434b415144c00fe0
jmackowiak <jmackowak@gmail.com>
2026-04-12 21:47:12 +0200

    git init

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black'
+  },
+  text: {
+    color: 'white'
+  }
+})
