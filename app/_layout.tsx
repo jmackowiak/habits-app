cdd488f6dce9eb56e7c3b68e6e99fb1eaa0107a4
jmackowiak <jmackowak@gmail.com>
2026-04-19 16:03:01 +0200

    add statusbar theme fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,24 +3,33 @@ import { StatusBar } from 'expo-status-bar'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { HabitsProvider } from '@/contexts/habits'
 import { ThemeProvider } from '@/contexts/theme'
+import { useAppTheme } from '@/hooks/useAppTheme'
+
+function AppContent() {
+	const { isDark } = useAppTheme()
+
+	return (
+		<GestureHandlerRootView>
+			<Stack>
+				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+				<Stack.Screen
+					name="new-habit"
+					options={{
+						presentation: 'fullScreenModal',
+						headerShown: false,
+					}}
+				/>
+			</Stack>
+			<StatusBar style={isDark ? 'light' : 'dark'} />
+		</GestureHandlerRootView>
+	)
+}
 
 export default function RootLayout() {
 	return (
 		<ThemeProvider>
 			<HabitsProvider>
-				<GestureHandlerRootView>
-					<Stack>
-						<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-						<Stack.Screen
-							name="new-habit"
-							options={{
-								presentation: 'fullScreenModal',
-								headerShown: false,
-							}}
-						/>
-					</Stack>
-					<StatusBar style="auto" />
-				</GestureHandlerRootView>
+				<AppContent />
 			</HabitsProvider>
 		</ThemeProvider>
 	)
